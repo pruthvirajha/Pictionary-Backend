0dc49ad864f6b1bdbb0e6f0f3b2ffe6991d25c61
pruthvirajha <pruthvirajha@gmail.com>
2020-08-03 21:02:09 +0530

    Initial project setup

---
+++ src/main/kotlin/com/white/leaf/pictionary/PictionaryApplication.kt
@@ -0,0 +1,12 @@
+package com.white.leaf.pictionary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PictionaryApplication
+
+fun main(args: Array<String>) {
+	runApplication<PictionaryApplication>(*args)
+	
+}
---
+++ src/test/kotlin/com/white/leaf/pictionary/PictionaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.white.leaf.pictionary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PictionaryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
