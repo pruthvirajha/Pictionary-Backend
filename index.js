4b759c49f38772ae0bb61abcd0498ed19527a509
Vignesh, Narayanasamy <vigneshnv7@gmail.com>
2020-08-07 22:40:19 +0530

    Adds socekt server, client implementation.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+    res.sendfile(__dirname + '/index.html');
 });
 
 http.listen(3000, () => {
     console.log('Listening on *: 3000');
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('chat_message', (message) => {
+        console.log('The message is: ' + message);
+    });
 });
