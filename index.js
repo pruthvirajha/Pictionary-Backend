8a17c1107dc186740cfd460393a25d9b7b091961
Pruthvirajha <pruthvirajha@gmail.com>
2020-08-10 22:08:15 +0530

    add/remove subscribers

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+
+var subscribers = new Set();
+
 app.get('/', (req, res) => {
     res.sendfile(__dirname + '/index.html');
 });
@@ -11,7 +14,15 @@ http.listen(3000, () => {
 });
 
 io.on('connection', (socket) => {
-    console.log('User connected');
+    let clientId = socket.client.id;
+
+    console.log('User connected with Id ' + clientId);
+    subscribers.add(clientId);
+
+    socket.on('disconnect', () => {
+        subscribers.delete(clientId);
+        console.log("subscribers after removal " + subscribers);
+    });
 
     socket.on('chat_message', (message) => {
         console.log('The message is: ' + message);
